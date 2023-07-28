84d9382a23ab6302175c65f180193a1ad5e83aff
ReeceWilkinson <reecetyler14@gmail.com>
2023-07-28 16:25:36 +0100

    mains
    main basics and folder systems for the project

---
+++ main.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow, ipcMain } = require('electron')
+const path = require('path')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    autoHideMenuBar: true,
+    width: 1000,
+    height: 800,
+    resizable: false,
+    center: true,
+    webPreferences: {
+        preload: path.join(__dirname, 'preload.js')
+      }
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+    ipcMain.handle('ping', () => 'pong')
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+          createWindow()
+        }
+      })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
