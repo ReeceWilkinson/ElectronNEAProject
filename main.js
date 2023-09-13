62b918ee242946b9513821b8fc6b31df103559b1
ReeceWilkinson <61788558+ReeceWilkinson@users.noreply.github.com>
2023-09-13 14:44:03 +0000

    updates

--- main.js
+++ main.js
@@ -11,29 +11,29 @@ const createWindow = () => {
     height: 800,
     resizable: false,
     center: true,
-    
+
     webPreferences: {
-        preload: path.join(__dirname, 'preload.js')
-      }
+      preload: path.join(__dirname, 'preload.js')
+    }
   })
 
   win.loadFile('index.html')
 }
 
 app.whenReady().then(() => {
-    ipcMain.handle('ping', () => 'pong')
-    createWindow()
+  ipcMain.handle('ping', () => 'pong')
+  createWindow()
 
-    app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) {
-          createWindow()
-        }
-      })
-      
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+
 })
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit()
-    }
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
 })
--- register.js
+++ register.js
@@ -2,36 +2,36 @@ const fs = require('fs/promises')
 const { writeFile, readFile } = require('fs');
 
 function submitRegData() {
-    const inputs = document.getElementById("regForm").elements;
+  const inputs = document.getElementById("regForm").elements;
 
-    let uname = inputs[0].value;
-    let fname = inputs[1].value;
-    let lname = inputs[2].value;
-    let pword = inputs[3].value;
+  let uname = inputs[0].value;
+  let fname = inputs[1].value;
+  let lname = inputs[2].value;
+  let pword = inputs[3].value;
 
-    var userObj = {
-        FirstName: fname,
-        LastName: lname,
-        UserName: uname,
-        Password: pword
-    };
+  var userObj = {
+    FirstName: fname,
+    LastName: lname,
+    UserName: uname,
+    Password: pword
+  };
 
-    let config = JSON.stringify(userObj)
-    const path = './users.json'
+  let config = JSON.stringify(userObj)
+  const path = './users.json'
 
-    readFile(path, (error, data) => {
-        if (error) {
-          console.log(error);
-          return;
-        }
-        const parsedData = JSON.parse(data);
-        parsedData.createdAt = new Date().toISOString();
-        writeFile(path, JSON.stringify(parsedData, null, 2), (err) => {
-          if (err) {
-            console.log('Failed to write updated data to file');
-            return;
-          }
-          console.log('Updated file successfully');
-        });
-      });
-  }
+  readFile(path, (error, data) => {
+    if (error) {
+      console.log(error);
+      return;
+    }
+    const parsedData = JSON.parse(data);
+    parsedData.createdAt = new Date().toISOString();
+    writeFile(path, JSON.stringify(parsedData, null, 2), (err) => {
+      if (err) {
+        console.log('Failed to write updated data to file');
+        return;
+      }
+      console.log('Updated file successfully');
+    });
+  });
+}
