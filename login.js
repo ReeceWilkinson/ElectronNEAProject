007a8a25a55501872c5e8b038e0041b89de8a110
ReeceWilkinson <61788558+ReeceWilkinson@users.noreply.github.com>
2023-10-04 12:51:25 +0000

    register form takes user input
    saving here as the form takes input but using require('fs') breaks it

---
+++ login.js
@@ -0,0 +1,8 @@
+function submitLoginData() {
+  const inputs = document.getElementById("logForm").elements;
+
+  let uname = inputs[0].value;
+  let pword = inputs[1].value;
+
+  
+}
--- register.js
+++ register.js
@@ -1,12 +1,10 @@
-const fs = require('fs');
-
 function submitRegData() {
   const inputs = document.getElementById("regForm").elements;
 
   let uname = inputs[0].value;
-  let fname = inputs[1].value;
-  let lname = inputs[2].value;
-  let pword = inputs[3].value;
+  let pword = inputs[1].value;
+  let fname = inputs[2].value;
+  let lname = inputs[3].value;
 
   var userObj = {
     FirstName: fname,
@@ -15,5 +13,6 @@ function submitRegData() {
     Password: pword
   };
 
+  document.getElementById('textTest').innerHTML = userObj;
   console.log(userObj)
 }
