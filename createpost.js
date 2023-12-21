1959f4976e9ac29fac8459cac6e167fcc0696508
ReeceWilkinson <reecetyler14@gmail.com>
2023-12-21 00:10:11 +0000

    literally just broke it more

--- createpost.js
+++ createpost.js
@@ -1,11 +1,5 @@
 const fs = require('fs')
-
-function imageToBase64(file) {
-    // read binary data
-    var bitmap = fs.readFileSync(file);
-    // convert binary data to base64 encoded string
-    return new bitmap.toString('base64');
-}
+const imageToBase64 = require('image-to-base64');
 
 function lengthChecker(title,text) {
     /**
@@ -58,21 +52,14 @@ function addPost() {
     var input = document.getElementById('imageInput');
 
     var file = input.files[0];
-    
+
     var currentUser = fs.readFileSync('currUser.txt','utf8')
 
-    var reader = new FileReader();
+    let outputString = ''
 
-    var base64String = ''
+    
 
-    reader.onload = function (e) {
-        base64String = e.target.result;
-
-        // You can use the base64String as needed.
-        return base64String
-    }
-
-    reader.readAsDataURL(file);
+    console.log(imageToBase64(file))
 
     var datetime = new Date(); 
 
@@ -85,7 +72,7 @@ function addPost() {
         time: datetime.toLocaleTimeString(),
         userPosted: currentUser,
         votes: 0,
-        pathToImage: base64String
+        pathToImage: file
     }
 
     if (lengthChecker(userTitle,userText) == true) {
